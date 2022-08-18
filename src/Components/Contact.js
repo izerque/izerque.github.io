2f946b5a81dbc86d66cca21cde413ee952698c6f
izerque <isaacmimo118@gmail.com>
2022-08-19 00:42:33 +0300

    Acivated Contact form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,34 +1,62 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+
 function Contact(){
+
+    const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_xcw5ptc', 'template_79r3gww', form.current, 'kgyLQyNjQOc82jnbY')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+
+
     return(
         <div className="w-full  box-border md:h-screen  ">
              
-            <div className="bg-white pt-10 flex flex-col md:flex-row md:items-center justify-start ">
+            <div className="bg-white pt-10 flex flex-col md:flex-row md:items-start justify-start ">
 
                 {/* Form Header */}
                <div className="md:w-1/2 md:justify-between">
             <h1 className="font-regular text-cyan-600 text-4xl p-4 m-0 leading-lose py-5">
                Contact Me
             </h1>
-            <p className= " w-4/5 text-gray-500 px-5 mt-0">
+            <p className= "w-full lg:w-4/5 text-gray-500 px-5 mt-0">
             I&apos;m interested in freelance opportunities &#8254; especially ambitious or large projects. However, if you have other request or question, don&apos;t hesitate to use the form.
 Marketing.
             </p>
             </div>
 
             {/* form section */}
-            <div className="md:w-1/2">
-            <form className="mt-5 p-5">
-            <input className="w-full p-5 h-10 m-1 font-normal text-gray-500 bg-gray-200" type="text" id="Name" name="Name" value="Name"></input>
-            <input className="w-full p-5 h-10  m-1 font-normal text-gray-500 bg-gray-200" type="text" id="html" name="fav_language" value="Email"></input><br></br>
-            <input className="w-full p-5 h-10  m-1 font-normal text-gray-500 bg-gray-200" type="text" id="Subject" name="Subject" value="Subject"></input>
-            <input className="w-full align-top p-5 h-36 m-1 font-normal text-gray-500 bg-gray-200" type="text" id="Message" name="Message" value="Message"></input>
-            </form>
+            <div className="md:w-1/2 p-5">
+            <form ref={form} onSubmit={sendEmail} className=""> 
+                {/* ref={form} onSubmit={sendEmail} some missing code */}
+      
+      <input type="text" name="user_name"  Value="Name" className="bg-gray-100 w-1/2 p-5 h-10" />
+    
+      <input type="email" name="user_email" Value="Email" className="bg-gray-100 w-1/2 p-5 h-10" />
+
+      <input type="text" name="subject" Value="Subject" className="bg-gray-100 w-full mt-2 p-5 h-10" />
+
+
+    
+      <textarea name="message" Value="Message" className='w-full bg-gray-100 h-40 mt-2 p-5' />
+      <input className=' w-auto border-2 mt-4  border-cyan-600  rounded-xl cursor-pointer   text-gray-700 font-light py-1  px-4' type="submit" value="Send Message" />
+    </form>
 
             
 
-            <button className=' w-auto border-2  border-cyan-600 ml-5 rounded-xl cursor-pointer   text-gray-700 font-light py-1  px-4' type="submit">
+            {/* <button  type="submit">
  Send Message
-</button>
+</button> */}
 </div>
 </div>
 <h1 className="font-bold  opacity-40 text-right text-gray-200 text-8xl align-top">
